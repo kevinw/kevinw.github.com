f5d59a0a4dcba27a2e2a670226e04c578ea0746c
Kevin Watters <kevinwatters@gmail.com>
2017-08-17 11:06:29 -0700

    Update traplands

--- games/traplands/assets/atlas.bat
+++
@@ -1,2 +0,0 @@
-@set ASEPRITE="C:\Program Files (x86)\Aseprite\aseprite.exe"
-%ASEPRITE% -b *.ase --sheet atlas.png --sheet-pack --data atlas.json
--- games/traplands/assets/sprites/atlas.bat
+++ games/traplands/assets/sprites/atlas.bat
@@ -5,5 +5,6 @@
 @set OUTPUT_PNG=%OUTPUT_NAME%.png
 @set OUTPUT_JSON=%OUTPUT_NAME%.json
 
+cd %~dp0
 %ASEPRITE% -b ase/*.ase --sheet %OUTPUT_PNG% --sheet-pack --data %OUTPUT_JSON% --list-tags
 optipng -o7 %OUTPUT_PNG%
